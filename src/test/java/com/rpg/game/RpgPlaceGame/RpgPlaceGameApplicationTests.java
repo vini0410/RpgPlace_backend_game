3a87a2d51b75772d50d98fa90239a6d1a949a605
vini <vini0420@gmail.com>
2023-08-31 20:48:07 -0300

    init

---
+++ src/test/java/com/rpg/game/RpgPlaceGame/RpgPlaceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.game.RpgPlaceGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgPlaceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

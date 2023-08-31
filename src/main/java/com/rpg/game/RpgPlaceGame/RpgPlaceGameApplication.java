330b021a909c1c43427eb7adf8c10ce0d0205690
vini <vini0420@gmail.com>
2023-08-31 20:48:07 -0300

    init

---
+++ src/main/java/com/rpg/game/RpgPlaceGame/RpgPlaceGameApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.game.RpgPlaceGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgPlaceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgPlaceGameApplication.class, args);
+	}
+
+}
